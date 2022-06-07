52cafbc412b6d1ffb1e1ee9d9420ce093839e4ba
Lysio4 <103937997+Lysio4@users.noreply.github.com>
2022-06-07 15:41:58 +0200

    Delete constantes.h

--- constantes.h
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-#define Taille 100
-#define Largeur 100
-#define Longueur 100
-    enum {VIDE = ' ', MANU='@', CAISSE='$', CIBLE='.', MUR='#', CAISSESURCIBLE='&', MANUSURCIBLE='!'};
-    enum {Haut, Bas, Droite , Gauche};
